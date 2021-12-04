6a326f6385339ce4a76673d220aac708333b6955
Fedir Vasyliuk <f.vasyliuk@apiko.com>
2021-12-04 17:31:41 +0200

    added Cat class

---
+++ java/com/fv/Home_3.java
@@ -0,0 +1,8 @@
+public class Home_3 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Сніжко", "white");
+        Cat cat2 = new Cat("Барсік", "red");
+        cat1.sayHello();
+        cat2.sayHello();
+    }
+}
