1dc5a0ddfc0b8274b206ee73e12bfda88e0da15d
Fedir Vasyliuk <fedron18@gmail.com>
2021-12-18 19:22:03 +0200

    home task 6

--- java/com/fv/Cat.java
+++ java/com/fv/Cat.java
@@ -1,6 +1,10 @@
+import java.util.Random;
+
 public class Cat {
+    private static int count;
     public String name;
     private String color;
+    private Owner owner;
     public Cat(String name){
         this.name = name;
     }
@@ -8,6 +12,8 @@ public class Cat {
     public Cat(String name, String color) {
         this.name = name;
         this.color = color;
+        this.owner = new Owner("incognito");
+        Counter.setCount();
     }
 
     public String getColor(){
@@ -21,4 +27,47 @@ public class Cat {
         String text = String.format("Hello! My name is %s. My color is %s", this.name, this.color);
         System.out.println(text);
     }
+
+    public void randomName(){
+        Random rand = new Random();
+        int value = rand.nextInt(4);
+        Names text;
+        switch(value){
+            case 1: 
+                text = Names.BODY;
+                break;
+            case 2: 
+                text = Names.SHARIK;
+                break;
+            case 3:
+                text = Names.ROKI;
+                break;
+            default:
+                text = Names.REMBO;
+        }
+        System.out.println(text);
+    }
+
+    private class Habitat {
+        private String habitat = "Earth";
+        private int countOfEarth = 100;
+    }
+
+    public class Owner {
+        private String name;
+        public Owner(String name) {
+            this.name = name;
+        }
+    }
+
+    public static class Counter {
+        private static void setCount() {
+            count++;
+        }
+    }
+}
+
+enum Names {
+    BODY, SHARIK, ROKI, REMBO
+
 }
